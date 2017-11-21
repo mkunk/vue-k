9c4b181beb4732c0a2cbed968c1e7d9ef9d4dbc5
e <l>
2017-11-21 17:47:28 +0800

    mkunk add component

---
+++ src/components/scanning.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {}
+        },
+        components: {},
+        computed: {}
+    }
+</script>
+
+<style>
+</style>
